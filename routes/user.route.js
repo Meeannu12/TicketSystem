e8104f4ca7fdf5cbd3016e223973f7c17b562824
AnuragLovesCode <anurag.neet01@gmail.com>
2025-04-18 17:47:34 +0530

    add console.log

--- routes/user.route.js
+++ routes/user.route.js
@@ -20,19 +20,21 @@ userRoutes.get("/neet", (req, res) => {
 
     const callData = {};
 
-    if (req.query.MOBILE)   callData.customerNumber = req.query.MOBILE;
-    if (req.query.VMN)    callData.virtualNumber = req.query.VMN;
-    if (req.query.TIME)  callData.startTime = req.query.TIME;
-    if (req.query.END)    callData.endTime = req.query.END;
+    if (req.query.MOBILE) callData.customerNumber = req.query.MOBILE;
+    if (req.query.VMN) callData.virtualNumber = req.query.VMN;
+    if (req.query.TIME) callData.startTime = req.query.TIME;
+    if (req.query.END) callData.endTime = req.query.END;
     if (req.query.DURATION) callData.duration = req.query.DURATION;
-    if (req.query.EXENO)  callData.executiveNumber = req.query.EXENO;
-    if (req.query.RFN)    callData.referenceNumber = req.query.RFN;
-    if (req.query.STATUS)   callData.status = req.query.STATUS;
-    if (req.query.REC)    callData.recording = req.query.REC;
+    if (req.query.EXENO) callData.executiveNumber = req.query.EXENO;
+    if (req.query.RFN) callData.referenceNumber = req.query.RFN;
+    if (req.query.STATUS) callData.status = req.query.STATUS;
+    if (req.query.REC) callData.recording = req.query.REC;
+
+    console.log("dataget", callData);
 
     res.status(200).json({
       message: "Received call data",
-      data: callData
+      data: callData,
     });
   } catch (error) {
     res.status(500).json({ message: error.message });
