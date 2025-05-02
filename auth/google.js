b6e214b8fcd9f235ac61006e70b47ad02f027aab
AnuragLovesCode <anurag.neet01@gmail.com>
2025-05-02 14:32:29 +0530

    again change callback url

--- auth/google.js
+++ auth/google.js
@@ -8,7 +8,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${api}/auth/google/callback`,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
     },
     (accessToken, refreshToken, profile, done) => {
       // You can save user info to DB here
