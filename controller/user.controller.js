b03493b07ad959f9fabc25e751591614bda2ec2a
AnuragLovesCode <anurag.neet01@gmail.com>
2025-05-07 19:01:45 +0530

    remove ternory oprator from member

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -153,7 +153,7 @@ const checkInUser = async (req, res) => {
       {
         checkIn: true,
         checkInTime: new Date(),
-        member: member.length > 0 ? member : [],
+        member: member,
       },
       { new: true }
     );
